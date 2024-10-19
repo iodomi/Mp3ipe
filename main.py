9b4e9dca9750047c6ff198130b455e264934d82a
iodomi <iodomi@proton.me>
2024-10-19 11:54:43 +0200

    merged search functions into find(), added a lot more comments, polished stuff...

--- main.py
+++ main.py
@@ -1,62 +1,69 @@
 from sys import argv
 import os
 
-# configuration:
-
-PATH = "/mnt/data/music"
+# config:
+PATH = "/mnt/data/music" # enter a full path to your existing music folder
 
 class Mp3ipe:
     def __init__(self, path) -> None:
+         # check if path is correct
         if not path and not os.path.exists(path):
             print("Invalid path to music directory!")
             exit()
 
+        # init arguments
         self.args = ' '.join(argv[1:])
 
-        if not self.args == "":
-            music_file = self.choose(self.dir_search(self.args, path) + self.file_search(self.args, path))
+        if not self.args == "": # play sound
+            music_file = self.choose(self.find(self.args, path)) # find a music file/folder
+            # play music file with mpv
             os.system(f'mpv "{music_file}" --display-tags=icy-title --no-audio-display --no-video')
-        else:
+        else: # show directory contents
             os.system(f"ls -uxN {path}")
 
-    def file_search(self, name, path):
-        results = []
+    def find(self, name, path):
+        final_file, final_dir = [], [] # init variables holding found music
+        
+        # walk thru entire music directory 
         for root, dirs, files in os.walk(path):
-            for file in files:
-                if name.lower() in file.lower():
-                    results.append(os.path.join(root, file))
-        return results
-
-    def dir_search(self, name, path):
-        results = []
-        for root, dirs, files in os.walk(path):
+            # directory
             for dir_name in dirs:
                 if name.lower() in dir_name.lower():
-                    results.append(os.path.join(root, dir_name))
-        return results
+                    final_dir.append(os.path.join(root, dir_name))
+            # file
+            for file_name in files:
+                if name.lower() in file_name.lower():
+                    final_file.append(os.path.join(root, file_name))
+        return final_dir + final_file # return path to a file/folder
 
     def choose(self, options):
+        # if the song was found at all
         if not options:
-            print("No results found...")
+            print("No results have been found...")
             exit()
 
+        # if only one song is matching
         if len(options) == 1:
+            print(f"Playing: {options[0].removeprefix(PATH)}...")
             return options[0]
 
+        # if there is more than one song matching
         print("Found multiple results:")
         for i, option in enumerate(options, 1):
+            # display matching songs in a order
             print(f"{i}. {option.removeprefix(PATH)}")
         
         while True:
             try:
+                # let the user choose a song they want
                 choice = int(input(f"Pick a number (1-{len(options)}): "))
                 if 1 <= choice <= len(options):
                     return options[choice - 1]
-                else:
-                    print(f"Invalid choice! Select a number between 1 and {len(options)}.")
-            except ValueError:
+                else: # if user picked a wrong number
+                    print(f"Invalid choice! Pick a number between 1 and {len(options)}.")
+            except ValueError: # if user picked invalid character
                 print("Please enter a valid number!")
-            except KeyboardInterrupt:
+            except KeyboardInterrupt: # if user forced quit
                 print(" (interrupted)")
                 break
 
